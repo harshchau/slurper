025d6adb5b051bc007da7e1acaa24b814d116f09
Harsh Chaudhary <dashersch@gmail.com>
2020-06-23 15:52:40 -0400

    Add multiple posts to a dictionary and return mkdwn as string

--- get.py
+++ get.py
@@ -6,16 +6,27 @@ def loadRSS(url):
     resp = requests.get(url)
     return resp
 
-def getItems(rssURL):
+def getItems(rssURL) -> dict:
     ns = {'dc': 'http://purl.org/dc/elements/1.1/', 
             'content': 'http://purl.org/rss/1.0/modules/content/'
         }
+    posts = {}
     tree = ET.fromstring(loadRSS(rssURL).text)
     for item in tree.iter('item'):
         title = item.find('title').text
         content = item.find('content:encoded', ns)
-        return html2text.html2text(content.text)
+        posts[title] = html2text.html2text(content.text)
+    
+    return posts
+
+def export(url: str):
+    mkdwn = ''
+    posts = getItems(url)
+    for title, content in posts.items():
+        mkdwn += '#' + title + '\n\n' + content
+
+    return mkdwn 
 
 
-mkdwn = getItems('https://medium.com/feed/@gavinlewis')
+mkdwn = export('https://medium.com/feed/@harshchau')
 print(mkdwn)
