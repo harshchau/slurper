eef0ac25934b78b7be8d7e45bfa6b100b2d709d5
Harsh Chaudhary <dashersch@gmail.com>
2020-06-23 23:38:43 -0400

    Added support for beautiful soup

--- get.py
+++ get.py
@@ -5,10 +5,12 @@ from sys import argv
 import logging 
 from bs4 import BeautifulSoup
 
+# TO BE DEPRECATED - RSS sucks for Medium
 def load_rss(url):
     resp = requests.get(url)
     return resp
 
+# TO BE DEPRECATED - RSS sucks for medium
 def get_rss_items(rssURL) -> dict:
     ns = {'dc': 'http://purl.org/dc/elements/1.1/', 
             'content': 'http://purl.org/rss/1.0/modules/content/'
@@ -25,6 +27,7 @@ def get_rss_items(rssURL) -> dict:
     
     return posts
 
+# TO BE DEPRECATED - RSS sucks for medium
 def export_rss():
     url = 'https://medium.com/feed/@'
     try:
@@ -39,5 +42,15 @@ def export_rss():
     
     print(mkdwn)
 
+def get_series(url:str) -> None:
+    html_doc = requests.get(url).text
+    soup = BeautifulSoup(html_doc, 'html.parser')
+    series_header = soup.find_all('h1')[0].get_text()
+    print(series_header)
+    main_html_node = soup.find_all(attrs={'id'})
+    #print(soup.prettify(main_html_node))
+    print((main_html_node))
+
 if __name__ == '__main__':
-    export_rss() 
+    #export_rss() 
+    get_series('https://medium.com/series/py-6c6c7d22788f')
