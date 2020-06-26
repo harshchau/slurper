39ce2dd88bfae84d23e8312c9e7afb9979fa27d1
Harsh Chaudhary <dashersch@gmail.com>
2020-06-26 02:19:01 -0400

    Use series URL instead of RSS to get markdown

--- get.py
+++ get.py
@@ -3,6 +3,7 @@ import xml.etree.ElementTree as ET
 import html2text
 from sys import argv
 import logging 
+import bs4
 from bs4 import BeautifulSoup
 
 # TO BE DEPRECATED - RSS sucks for Medium
@@ -45,11 +46,41 @@ def export_rss():
 def get_series(url:str) -> None:
     html_doc = requests.get(url).text
     soup = BeautifulSoup(html_doc, 'html.parser')
-    series_header = soup.find_all('h1')[0].get_text()
-    print(series_header)
-    main_html_node = soup.find_all(attrs={'id'})
-    #print(soup.prettify(main_html_node))
-    print((main_html_node))
+    sections = soup.find_all('section')
+    for section in sections:
+        #content = section.find_all(id = True)
+        #c = section.findChildren(id = True)
+        #print(section)
+        section.attrs = None
+        #print(section)
+        [make_attr_none(d) for d in section.descendants if d is not bs4.element.NavigableString]
+        a = html2text.html2text(str(section))
+        print(a)
+        
+def make_attr_none(tag):
+    if tag.name == 'img': # For img tags
+        # Get the dict of tag.attrs where attribute is srcset and the parent is a div
+        # We use parent = div to filter out the img tags under noscript
+        tag.attrs = {k:v for k,v in tag.attrs.items() if k == 'srcset' and tag.parent.name == 'div'} 
+        if('srcset' in tag.attrs): 
+            # Rename the srcset key to src
+            tag.attrs['src'] = tag.attrs['srcset']
+            del tag.attrs['srcset']
+            # Convert src values to a list
+            a = [tag.attrs['src']]
+            a = a[0].split(',') # Split on the ,
+            # Split on " " to tokenize the width text such as 500w
+            # Take the first element of the sub-array which will be the URL and 
+            # discard the width such as 500w
+            a = [l.strip().split(' ')[0] for l in a]
+            # Pick the last URL, the assumption is that the last URL will be the 
+            # largest image
+            a = a[-1]
+            # Set the src attribute to the last URL
+            tag.attrs['src'] = a
+    else:
+        tag.attrs = None # Remove all attributes
+    return tag
 
 if __name__ == '__main__':
     #export_rss() 
