a98cea7dabe429ca8575195ba0229b32a412e043
Harsh Chaudhary <dashersch@gmail.com>
2020-07-03 00:03:11 -0400

    Updated setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="slurper", # Replace with your own username
+    version="0.0.2",
+    author="Harsh Chaudhary",
+    author_email="chaudhary.harsh@gmail.com",
+    description="A package to scrape public data sources",
+    long_description=A package to scrape public data sources,
+    long_description_content_type="text/markdown",
+    url="https://github.com/harshchau/slurp",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
