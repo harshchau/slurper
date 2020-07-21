fdfcf4c15adfeb662f0758406de0f31a7da52c61
Harsh Chaudhary <dashersch@gmail.com>
2020-07-21 18:18:52 -0400

    Added robots checker

--- slurper/url.py
+++ slurper/url.py
@@ -1,11 +1,10 @@
-#import urllib 
-#from urllib.parse import urlparse
 from dataclasses import dataclass 
 from datetime import datetime
 from datetime import date
 import tldextract 
 from json import JSONEncoder
-import urllib.robotparser 
+import requests
+from reppy.robots import Robots
 
 
 '''
@@ -13,24 +12,18 @@ Module to handle url requests.
 - Parse incoming URL 
 - Construct a URL object as shown below 
 
-url:
-    - url
-    - domain 
-    - type: (post, publication)
-    - date of processing
-    - name of requestor 
-
 '''
 
 
 @dataclass
 class URL:
     url: str
+    scheme: str 
     hostname: str
     domain: str 
     subdomain: str 
-    tld: str
-    type: str 
+    suffix: str
+    url_type: str 
     time_requested: datetime 
     requesting_user: str 
 
@@ -46,7 +39,7 @@ class url_parser:
             urls = list(urls) # Converting to list to access pop 
         else:
             urls = urls[0]
-        print('>>>>>', urls)
+        #print('>>>>>', urls)
         urls.reverse() # eversing to maintain order in which user sent the list as this will be reversed back during popping
         #print(type(urls), urls)
         while len(urls) > 0:
@@ -62,21 +55,26 @@ class url_parser:
     '''
     def process_url(self, url: str) -> URL:
         extract = tldextract.extract(url)
+        # Find the first non None part in the url and do a substring on the url from index 0 - whatever
+        scheme = url[:url.index([part for part in extract if part][0])]
         subdomain = extract.subdomain
         domain = extract.domain 
         suffix = extract.suffix
         hostname = '.'.join(part for part in extract if part)
-        type = 'PUB' if subdomain else 'POST'
+        url_type = 'PUB' if subdomain else 'POST'
         time_requested = datetime.now().timestamp() * 1000
         requesting_user = None
 
-        #rp = urllib.robotparser.RobotFileParser()
-        #rp.set_url('https://' + hostname + '/robots.txt')
-        #rp.read()
-
-        url = URL(url, hostname, domain, subdomain, suffix, type, time_requested, requesting_user)
+        url = URL(url, scheme, hostname, domain, subdomain, suffix, url_type, time_requested, requesting_user)
 
         return url
+
+    def can_crawl(self, url: str) -> bool:
+        ret = False
+        robots_url = Robots.robots_url(url)
+        robots = Robots.fetch(robots_url, headers={'user-agent': 'slurper'})
+        ret = robots.allowed(url, 'slurper')
+        return ret
     
 class SeriesEncoder(JSONEncoder):
     def default(self, o):
@@ -87,11 +85,10 @@ class SeriesEncoder(JSONEncoder):
 if __name__ == "__main__":
 
     s1 = "https://marker.medium.com"
-    s2 = "https://marker.com"
+    s2 = "https://medium.com"
     l = []
     l.append(s1)
     l.append(s2)
     url = url_parser().parse(l)
-    print(url)
     print(SeriesEncoder().encode(url))
 
