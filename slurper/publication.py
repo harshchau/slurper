6ef765a299e32f7d49b7646ce0fcd0296a5e0f68
Harsh Chaudhary <dashersch@gmail.com>
2020-07-22 21:02:55 -0400

    Cleanup

--- slurper/publication.py
+++ slurper/publication.py
@@ -1,105 +1,127 @@
-import scrapy
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
 import logging
-from scrapy.crawler import CrawlerProcess
-#from lxml import etree
+import argparse
+from bs4 import BeautifulSoup
+import time
+from url import url_parser
+from urllib.parse import urlparse
 
 '''
-Take a publication URL and extract 
-- tags
-- editors
-- authors
-- URLs
-To run: `scrapy runspider publication.py --nolog`
+Use a headless browser to navigate a series or post and get the entire dataset
+Steps:
+    # Set up headless browser
+    # Get initial dataset
+    # Click on last element to get the next 
+    # Add the newly received element to the initial dataset 
+    # Repeat 
 '''
 
-logging.basicConfig(
-    filename='log.txt',
-    format='%(levelname)s: %(message)s',
-    level=logging.ERROR
-)
+# args config 
+#parser = argparse.ArgumentParser()
+#parser.add_argument('-url', help = 'URL of the medium series', type = str)
+#args = parser.parse_args()
 
+# source url
+#url = args.url # Sample URL > https://medium.com/series/sample-3d219d98b481
+
+#if url is None: url = 'https://medium.com/series/sample-3d219d98b481'
+
+# Logging config 
+logging.basicConfig(level = logging.ERROR)
 log = logging.getLogger(__name__)
-log.setLevel(logging.INFO)
+logging.getLogger(__name__).setLevel(logging.DEBUG)
 
-class Publication(scrapy.Spider):
-    name = 'tags'
-    start_urls = [
-        'https://marker.medium.com/archive',
-    ]
+# Set up headless browser 
+chrome_options = webdriver.ChromeOptions() 
+chrome_options.add_argument("--headless") 
+browser = webdriver.Chrome(options=chrome_options)
 
-    '''
-    - Tags
-        - Find a header tag that contains "Tags"
-        - Find all siblings with an index
-        - Find the sibling that contains "Editors". i-1 will bewhere the tags end
-    '''
-    def parse(self, response):
-        tags = self.tags(response)
-        editors = self.editors(response)
-        writers = self.writers(response)
-        print(tags)
-        #print(editors)
-        #print(writers)
+# Get initial dataset 
+def get_initial_payload(url: str):
+    log.debug(f'URL > {url}')
+    browser.get(url)
+    elements = browser.find_elements_by_tag_name('section')
+    #log.debug(f'Found {len(elements)} elements')
 
+    return elements
 
-    def writers(self, response):
-        writers = {}
-        start_selector = response.xpath('//header[descendant::text() = "Writers"]/following-sibling::*') # Start of Editors + following siblings
-        # Stop selector not required as there are no non-writer elemtns following the start selector list 
-        #stop_selector = response.xpath('//header[descendant::text() = "Writers"]/parent::*')[0] # Start of Writers
+def iterate_a(element_list: list, text_element_list: list = []) -> list:
+    if element_list[-1] == element_list[-2]: 
+        #log.debug(f'ENDING ..... found {len(element_list)} elements')
 
-        for sibling in start_selector:
-            writers[sibling.xpath('./div/a/text()').get()] = sibling.xpath('./div/a/@href').get()
+        # Drop the last element before returning because when the else reaches 
+        # the end of the content, it still add the last element to the 
+        # element_list. In other words, we will add the last element to the 
+        # list and then compare to previous element in the next step. This 
+        # comparison then tells us whether or not thr elements are equal
+        return element_list[:-1], text_element_list[:-1]
+    else:
+        log.debug(len(element_list))
+        click(element_list[-1])
+        element_list.append(browser.find_elements_by_tag_name('section')[-1])
+        text_element_list.append(get_element_as_text(element_list[-1]))
 
-        return writers
+        return iterate_a(element_list, text_element_list)
 
-    def editors(self, response):
-        editors = {}
-        start_selector = response.xpath('//header[descendant::text() = "Editors"]/parent::*/following-sibling::*') # Start of Editors + following siblings
-        stop_selector = response.xpath('//header[descendant::text() = "Writers"]/parent::*')[0] # Start of Writers
+def click(element):
+    #e = element.find_elements_by_tag_name('div')[-1]
+    #action = ActionChains(browser)
+    #action.move_to_element(e).click().perform()
+    browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+    time.sleep(3)
 
-        for sibling in start_selector:
-            if stop_selector.get() == sibling.get():
-                log.info('Found STOP selector for editors .... stopping')
-                break
-            else:
-                editors[sibling.xpath('.//a/text()').get()] = sibling.xpath('.//a/@href').get()
+def get_element_as_text(element):
+    s = None
+    try:
+        s = element.get_attribute('outerHTML')
+    except selenium.common.exceptions.StaleElementReferenceException as error:
+        # Just log and don't take action as the text for stale elements has been
+        # captured in the text_element list 
+        log.error(f'IGNORE > {error.msg}')
 
-        return editors
+    return s
 
+def get_urls(text_elements: list) -> list:
+    ret = []
+    for e in text_elements:
+        soup = BeautifulSoup(e, 'html.parser')
+        for a in soup.find_all('a'):
+            ret.append(a['href'])
+    return ret 
 
-    def tags(self, response) -> dict: 
-        tags = {}
-        
-        start_selector = response.xpath('//header[descendant::text() = "Tags"]/following-sibling::*') # Start of Tags + following siblings
-        stop_selector = response.xpath('//header[descendant::text() = "Editors"]/parent::*')[0] # Start of editors
+def remove_author_urls(urls_list: list) -> list:
+    ret = []
+    up = url_parser()
+    for u in urls_list:
+        #url_parts = up.process_url(u)
+        #print(u[u.index(url_parts.suffix):])
+        path = urlparse(u).path
+        if path[1:].startswith('@'): continue # Don't add user URL's
+        ret.append(u)
 
-        for sibling in start_selector:
-            if stop_selector.get() == sibling.get():
-                log.info('Found STOP selector for tags ..... stopping')
-                break
-            else:
-                tags[sibling.xpath('./a/text()').get()] = sibling.xpath('./a/@href').get()
+    return ret  
 
-        #tags = {for sibling in start_selector if stop_selector.get() != sibling.get()}
-            
-        return tags
+def get_content(url: str): 
+    elems = get_initial_payload(url)
+    text_elements = [get_element_as_text(e) for e in elems]
+    #log.info(f'Found {len(elems)} elements on inital run')
+    elems, text_elements = iterate_a(elems, text_elements)
+    #log.info(f'Found {len(text_elements)} elements and {len(elems)} text elements')
+    #log.info(text_elements[0])
+    #urls = set(get_urls(text_elements) ) # Remove duplicates
+    urls = []
+    [urls.append(x) for x in get_urls(text_elements) if x not in urls] # Remove duplicates this way to maintain order of urls
+    urls = remove_author_urls(urls)
+    for u in urls:
+        log.info(u)
+    log.debug(f'{len(urls)} urls found')
+
+    return text_elements
 
 if __name__ == '__main__':
-    process = CrawlerProcess({
-        
-    })
-    process.crawl(Publication)
-    process.start()
-        
-
-
-
-
-
-
-
-
-
-# response.xpath('//section/descendant::text()').getall()
-#response.xpath('//header[descendant::text() = "Tags"] | //header[descendant::text() = "Editors"] | //header[descendant::text() = "Writers"]').getall()
+    #url = 'https://medium.com/series/sample-3d219d98b481'
+    url = 'https://marker.medium.com'
+    get_content(url)
