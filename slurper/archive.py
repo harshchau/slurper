9abfc7311ca942edb9715514e199623246bc3a96
Harsh Chaudhary <dashersch@gmail.com>
2020-08-10 00:06:22 -0400

    Handle case where invalid initial url is provided

--- slurper/archive.py
+++ slurper/archive.py
@@ -27,6 +27,12 @@ class ArchiveProcessor:
         # make some soup
         self.soup = BeautifulSoup(html_doc, 'html.parser')
         self.tracker_list.update({archive_url:self.get_url_info(archive_url)['key']})
+        # This check is required only on the initial URL. This is due to the behavior of medium
+        # where for an archive, a wrong url still returns the archive page for the url.
+        # This check is not required for right initial urls as that check is done when 
+        # local_list is created 
+        if self.is_url_valid(archive_url) is False:
+            raise Exception(f'Invalid URL: {archive_url}')
 
     def get_timebuckets(self, url_list): 
 #        print(url_list)
@@ -34,9 +40,9 @@ class ArchiveProcessor:
         # If url is a date url, return. At this point, all peer date urls have been added to url_list and tracker_list
         if self.get_url_info(u)['is_date_url']:
             if len(url_list) == 0: # To prevent breaks when starting input is a date url only (url_list will be empty)
-                return self.tracker_list
+                return self.tracker_list # Done 
             else:
-                return self.get_timebuckets(url_list)
+                return self.get_timebuckets(url_list) # Keep going 
         html_doc = requests.get(u).text
         soup = BeautifulSoup(html_doc, 'html.parser')
         timeline_tags = soup.find_all('div', class_='timebucket')
@@ -48,9 +54,9 @@ class ArchiveProcessor:
         if len(url_list) == 0:
 #            for k,v in self.tracker_list.items():
 #                print(k,v)
-            return self.tracker_list
+            return self.tracker_list # Done 
         else:
-            return self.get_timebuckets(list(sorted(set(url_list))))
+            return self.get_timebuckets(list(sorted(set(url_list)))) # Keep going 
 
     def get_url_info(self, url: str):
         url_info = {'is_date_url': False}
@@ -66,6 +72,21 @@ class ArchiveProcessor:
         url_info['key'] = '#'.join(split_rest[1:])
 
         return url_info
+
+    def is_url_valid(self, url):
+        ret = False 
+        html_doc = requests.get(url).text
+        soup = BeautifulSoup(html_doc, 'html.parser')
+        timeline_tags = soup.find_all('div', class_='timebucket')
+        local_list = [t.a['href'] for t in timeline_tags if t.a]
+        try:
+            key_list = [self.get_url_info(u)['key'] for u in local_list]
+            input_url_key = self.get_url_info(url)['key']
+            if len([k for k in key_list if input_url_key in k]) > 0: ret = True
+        except ValueError as err:
+            print(err)
+
+        return ret
 
     '''
         Given a URL to a publication archive (yearly, monthly, daily), get all post URL's from the archives
@@ -85,7 +106,7 @@ class ArchiveEncoder(JSONEncoder):
         return ret 
 
 if __name__ == '__main__':
-    archive_url = 'https://marker.medium.com/archive/2019/11/'
+    archive_url = 'https://marker.medium.com/archive'
     ap = ArchiveProcessor(archive_url)
     ap.timebuckets = ap.get_timebuckets([archive_url])
     print(json.dumps(ap.timebuckets, cls=ArchiveEncoder, indent=2))
