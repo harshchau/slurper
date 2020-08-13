882aa8e490d66b0a09be36ed64e74972734f9941
Harsh Chaudhary <dashersch@gmail.com>
2020-08-13 17:08:56 -0400

    Fixed is_url_valid

--- slurper/archive.py
+++ slurper/archive.py
@@ -54,15 +54,15 @@ class ArchiveProcessor:
             self.tracker.update({archive_url:{'key':self.get_url_info(archive_url)['key'], 'post-urls':self.get_archive_post_urls(archive_url)}})
         self.ex = futures.ThreadPoolExecutor(max_workers=20) # 20 workers seems to be ideal
 
-    @_timer_function_run
+#    @_timer_function_run
     def get_timebuckets(self, url_list): 
-        print(url_list)
+    #    print(url_list)
         u = url_list.pop()
 
         # If url is a date url, return. At this point, all peer date urls have been added to url_list and tracker
         if self.get_url_info(u)['is_date_url']:
-            print('GETTING 1')
             if len(url_list) == 0: # To prevent breaks when starting input is a date url only (url_list will be empty)
+                print('GETTING 1')
                 return self.tracker # Done 
             else:
                 print('GETTING 2')
@@ -70,17 +70,19 @@ class ArchiveProcessor:
 
 #        print(f'LOCAL_LIST: {len(local_list)} URL_LIST: {len(url_list)} TRACKER: {len(self.tracker)} URL: {u}')
 
-
         local_list = self._get_child_calendar_urls(u)
+
+        ################
         # Concurrent calls to get archive post urls for local list
-        archive_post_urls_for_local_list = self.ex.map(self.get_archive_post_urls, local_list)
-        real_results = list(archive_post_urls_for_local_list)
+##        archive_post_urls_for_local_list = self.ex.map(self.get_archive_post_urls, local_list)
+##        real_results = list(archive_post_urls_for_local_list)
         # Store all archive post urls for each url in the local list in a dictionary
-        dict_of_archive_post_urls_for_local_list = {u:real_results[local_list.index(u)] for u in local_list}
+##        dict_of_archive_post_urls_for_local_list = {u:real_results[local_list.index(u)] for u in local_list}
+        ################
         
 
         url_list.extend(local_list)
-        self.tracker.update({u:{'key':self.get_url_info(u)['key'],'post-urls':dict_of_archive_post_urls_for_local_list[u]} for u in local_list})
+        self.tracker.update({u:{'key':self.get_url_info(u)['key'],'post-urls':self.get_archive_post_urls(u)} for u in local_list})
 
         if len(url_list) == 0:
             return self.tracker # Done 
@@ -88,12 +90,11 @@ class ArchiveProcessor:
             return self.get_timebuckets(list(sorted(set(url_list)))) # Keep going 
 
     def _get_child_calendar_urls(self, url):
-
         html_doc = requests.get(url).text
         soup = BeautifulSoup(html_doc, 'html.parser')
         timeline_tags = soup.find_all('div', class_='timebucket')
         local_list = [t.a['href'] for t in timeline_tags if t.a]
-        print('>>>>>', local_list)
+#        print('>>>>>', local_list)
 
         return local_list
 
@@ -109,19 +110,35 @@ class ArchiveProcessor:
         url_info['key'] = '#'.join(split_rest[1:])
 
         return url_info
-    # Just called during __init__
+    
+    # Just called during __init__ but is called multiple times from the Lambda
     def is_url_valid(self, url):
         ret = False 
-        html_doc = requests.get(url).text
-        soup = BeautifulSoup(html_doc, 'html.parser')
-        timeline_tags = soup.find_all('div', class_='timebucket')
-        local_list = [t.a['href'] for t in timeline_tags if t.a]
-        try:
-            key_list = [self.get_url_info(u)['key'] for u in local_list]
-            input_url_key = self.get_url_info(url)['key']
-            if len([k for k in key_list if input_url_key in k]) > 0: ret = True
-        except ValueError as err:
-            print(err)
+        response = requests.get(url)
+        last_resp = response.history[-1]
+        print('RESPONSE', last_resp.status_code, last_resp.url, response.history)
+        # Just this is not enough. I can send in a date like 2019/09/99 and that is valid
+        # but 2019/09/100 is invalid
+        if last_resp.status_code == 301: # 2019/09/100
+            ret = False 
+        elif last_resp.status_code == 302 and len(response.history) > 2: # 2019/09/50
+            ret = False 
+        else:
+            ret = True
+#        html_doc = response.text
+#        soup = BeautifulSoup(html_doc, 'html.parser')
+#        timeline_tags = soup.find_all('div', class_='timebucket')
+#        local_list = [t.a['href'] for t in timeline_tags if t.a]
+#        try:
+#            key_list = [self.get_url_info(u)['key'] for u in local_list]
+#            input_url_key = self.get_url_info(url)['key']
+#            print('#####', type(input_url_key), input_url_key)
+#            print('#####', type(key_list), type(key_list[0]), key_list)
+            # Using in becase we want to test for whether 2020/08 (month) also exists in 
+            # 2020/08/03 (day)
+#            if len([k for k in key_list if input_url_key in k]) > 0: ret = True 
+#        except ValueError as err:
+#            print(err)
 
         return ret
 
@@ -129,9 +146,10 @@ class ArchiveProcessor:
         Given a URL to a publication archive (yearly, monthly, daily), get all post URL's from the archives
     '''
 #    @_timer_function_run
-    def get_archive_post_urls(self, archive_url: str) -> list:
+    def get_archive_post_urls(self, archive_url: str, soup = None) -> list:
         ret = []
-        html_doc = requests.get(archive_url).text
+        response = requests.get(archive_url)
+        html_doc = response.text
         soup = BeautifulSoup(html_doc, 'html.parser')
         sections = soup.find_all('section')
         for s in sections:
@@ -147,7 +165,7 @@ class ArchiveEncoder(JSONEncoder):
         return ret 
 
 if __name__ == '__main__':
-    archive_url = 'https://marker.medium.com/archive/2020/08'
+    archive_url = 'https://marker.medium.com/archive/2019/05'
     ap = ArchiveProcessor(archive_url)
     ap.timebuckets = ap.get_timebuckets([archive_url])
     print(json.dumps(ap.timebuckets, cls=ArchiveEncoder, indent=2))
